15820295d29b1126dd197895449a80492a36682c
Pinky-cloud224 <pinkydebnath4@gmail.com>
2020-10-09 13:02:37 +0600

    Fifth

---
+++ array_string3.py
@@ -0,0 +1,12 @@
+arr1 = [
+    1, 2, 3,
+    [
+        "A", "B",
+        [
+            "string1", "string2", "string4"
+        ]
+    ]
+]
+
+arr1.pop()
+print("Removing the last element", arr1)
