8deeec1f514be9397b6f9174b0c3d6290412a9c6
Pinky-cloud224 <pinkydebnath4@gmail.com>
2020-10-10 10:54:22 +0600

    First Commit

---
+++ assignment_5_6.py
@@ -0,0 +1,16 @@
+arr1 = [
+    1, 2, 3,
+    [
+        "A", "B",
+        [
+            "string1", "string2", "string4"
+        ]
+    ]
+]
+
+arry=arr1[3][2].pop(2)
+print(arr1)
+
+array=arr1[3][2].append("string5")
+print(arr1)
+
